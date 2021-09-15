7a518b0fe3867bc0d465f66e237c0976caa2edba
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-16 00:40:52 +0530

    feat: dom selection

--- stocks.js
+++ stocks.js
@@ -1,1 +1,7 @@
-console.log("gnotngkdjfn")
+const initialPrice  = document.querySelector("#initial-price")
+const stockQty = document.querySelector("#stock-qty")
+const finalPrice = document.querySelector("#final-price")
+const calcBtn = document.querySelector(".calc-btn")
+const outputDiv = document.querySelector(".output-div")
+
+console.log(initialPrice,stockQty,finalPrice,calcBtn,outputDiv)
