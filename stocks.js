5d259dae76afb8c4a22fde729ab6538e6cf54624
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-16 00:55:54 +0530

    feat: EventListener added

--- stocks.js
+++ stocks.js
@@ -4,4 +4,10 @@ const finalPrice = document.querySelector("#final-price")
 const calcBtn = document.querySelector(".calc-btn")
 const outputDiv = document.querySelector(".output-div")
 
-console.log(initialPrice,stockQty,finalPrice,calcBtn,outputDiv)
+console.log(initialPrice,stockQty,finalPrice,calcBtn,outputDiv);
+
+calcBtn.addEventListener("click",clickHandler);
+
+function clickHandler(){
+    console.log("hello")
+}
