9ffecc76dfc4eab00da474de97eecc2c30ce1ed7
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-16 02:13:55 +0530

    feat: inputValue() added

--- stocks.js
+++ stocks.js
@@ -4,10 +4,33 @@ const finalPrice = document.querySelector("#final-price")
 const calcBtn = document.querySelector(".calc-btn")
 const outputDiv = document.querySelector(".output-div")
 
-console.log(initialPrice,stockQty,finalPrice,calcBtn,outputDiv);
 
 calcBtn.addEventListener("click",clickHandler);
 
 function clickHandler(){
-    console.log("hello")
+  const inputs = inputValue() ;
+  
+  console.log(inputs.initialPriceVal,inputs.stockQtyVal,inputs.finalPriceVal)
+  
+
+//   console.log(inputs[0],inputs[1],inputs[2])   // to console log with inputVal
 }
+
+
+function inputValue(){
+    return{
+        initialPriceVal: initialPrice.value ,
+        stockQtyVal: stockQty.value ,
+        finalPriceVal: finalPrice.value ,
+    }
+}
+
+
+// function inputVal(){             // alternate function to take input values
+//    var initialPriceVal=initialPrice.value ;
+//    var stockQtyVal= stockQty.value ;
+//    var finalPriceVal=finalPrice.value;
+   
+// return [initialPriceVal, stockQtyVal, finalPriceVal] // returning values inside an array
+
+// }
