3b71fe8a53635d8af7b8bc40904bcb6fb8b157ab
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-24 03:47:33 +0530

    feat: base css added

--- stocks.js
+++ stocks.js
@@ -1,82 +1,89 @@
-const initialPrice  = document.querySelector("#initial-price")
-const stockQty = document.querySelector("#stock-qty")
-const finalPrice = document.querySelector("#final-price")
-const calcBtn = document.querySelector(".calc-btn")
-const outputDiv = document.querySelector(".output-div")
+const initialPrice = document.querySelector("#initial-price");
+const stockQty = document.querySelector("#stock-qty");
+const finalPrice = document.querySelector("#final-price");
+const calcBtn = document.querySelector(".calc-btn");
+const outputDiv = document.querySelector(".output-div");
 
+calcBtn.addEventListener("click", clickHandler);
 
-calcBtn.addEventListener("click",clickHandler);
+function clickHandler() {
+  const inputs = inputValue();
+  if (validateInput(inputs)) {
+    const result = calculateResult(inputs);
+    output(result);
+  } else {
+    validationAlert();
+  }
 
-function clickHandler(){
-const inputs = inputValue() ;
-if (validateInput(inputs)) {
-const result =calculateResult(inputs);
-output(result)
- }
-else {
-validationAlert()
+  //   console.log(inputs[0],inputs[1],inputs[2])   // to console log with inputVal
 }
 
-//   console.log(inputs[0],inputs[1],inputs[2])   // to console log with inputVal
+function inputValue() {
+  return {
+    initialPriceVal: parseFloat(initialPrice.value),
+    stockQtyVal: parseFloat(stockQty.value),
+    finalPriceVal: parseFloat(finalPrice.value),
+  };
 }
 
+function validateInput(inputs) {
+  if (
+    isNaN(inputs.initialPriceVal) ||
+    isNaN(inputs.stockQtyVal) ||
+    isNaN(inputs.finalPriceVal)
+  )
+    return false;
+  if (
+    inputs.initialPriceVal <= 0 ||
+    inputs.stockQtyVal <= 0 ||
+    inputs.finalPriceVal <= 0
+  )
+    return false;
+  return true;
+}
 
-function inputValue(){
-    return{
-        initialPriceVal:parseFloat( initialPrice.value) ,
-        stockQtyVal: parseFloat(stockQty.value) ,
-        finalPriceVal: parseFloat(finalPrice.value) ,
+function validationAlert() {
+  outputDiv.innerText =
+    "Enter input values greater than 0 and fill all input tabs";
+}
+
+function calculateResult(inputs) {
+  const initialBuy = inputs.initialPriceVal * inputs.stockQtyVal;
+  const finalBuy = inputs.stockQtyVal * inputs.finalPriceVal;
+  const results = finalBuy - initialBuy;
+  const resultPerc = (results / initialBuy) * 100;
+
+  return { results, resultPerc };
+}
+
+function output(res) {
+  document.body.style.backgroundColor = "white";
+  if (res.results > 0) {
+    if (res.resultPerc > 50) {
+      document.body.style.backgroundColor = "green";
     }
+    outputDiv.innerText = `The profit is ${res.results.toFixed(
+      2
+    )} and percentage of profit is ${res.resultPerc.toFixed(2)}`;
+  } else if (res.results < 0) {
+    res.results = res.results * -1;
+    res.resultPerc = res.resultPerc * -1;
+    if (res.resultPerc > 50) {
+      document.body.style.backgroundColor = "red";
+    }
+    outputDiv.innerText = `The loss is ${res.results.toFixed(
+      2
+    )} and loss in Percentage is ${res.resultPerc.toFixed(2)}`;
+  } else {
+    outputDiv.innerText = `You have no Loss or Profit!`;
+  }
 }
-
-function validateInput(inputs){
-if(isNaN(inputs.initialPriceVal) || isNaN(inputs.stockQtyVal) || isNaN(inputs.finalPriceVal) )
-return false;
-if (inputs.initialPriceVal<=0 || inputs.stockQtyVal<=0 || inputs.finalPriceVal<=0 )
-return false;
-return true;   
-}
-
-function validationAlert(){
-    outputDiv.innerText = "Enter input values greater than 0 and fill all input tabs"
-}
-
-function calculateResult(inputs){
-   const initialBuy =inputs.initialPriceVal*inputs.stockQtyVal
-   const finalBuy = inputs.stockQtyVal*inputs.finalPriceVal
-   const results = finalBuy-initialBuy
-   const resultPerc = results/initialBuy* 100;
-
-return { results , resultPerc }
-}
-
-function output(res){
-    document.body.style.backgroundColor= 'white' ;
-    if (res.results > 0) {
-    if (res.resultPerc>50) {
-        document.body.style.backgroundColor= 'green' ;
-    }   
-    outputDiv.innerText= `the profit is ${res.results.toFixed(2)} and percentage ${res.resultPerc.toFixed(2)}` ;}
-
-    else if(res.results < 0) {
-        res.results=res.results*(-1);
-        res.resultPerc = res.resultPerc*(-1);
-        if (res.resultPerc>50) {
-            document.body.style.backgroundColor= 'red' ;
-        }   
-        outputDiv.innerText= `the loss is ${res.results.toFixed(2)} and percentage ${res.resultPerc.toFixed(2)}` ;}
-
-    else {
-        outputDiv.innerText= `you have no loss or profit`}
-}
-
-
 
 // function inputVal(){             // alternate function to take input values
 //    var initialPriceVal=initialPrice.value ;
 //    var stockQtyVal= stockQty.value ;
 //    var finalPriceVal=finalPrice.value;
-   
+
 // return [initialPriceVal, stockQtyVal, finalPriceVal] // returning values inside an array
 
 // }
